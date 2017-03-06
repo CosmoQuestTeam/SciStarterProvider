e65f1a5850adffa56583b67bc4e619d395539b2c
Ryan Owens <RyanOwens@linux.com>
2017-03-06 13:35:11 -0600

    Creating new GitHub Repository

---
+++ README.md
@@ -0,0 +1,11 @@
+# SciStarter OAuth2 Provider for Laravel Socialite
+
+[![Scrutinizer Code Quality](https://img.shields.io/scrutinizer/g/SocialiteProviders/SciStarter.svg?style=flat-square)](https://scrutinizer-ci.com/g/SocialiteProviders/SciStarter/?branch=master)
+[![Latest Stable Version](https://img.shields.io/packagist/v/socialiteproviders/scistarter.svg?style=flat-square)](https://packagist.org/packages/socialiteproviders/scistarter)
+[![Total Downloads](https://img.shields.io/packagist/dt/socialiteproviders/scistarter.svg?style=flat-square)](https://packagist.org/packages/socialiteproviders/scistarter)
+[![Latest Unstable Version](https://img.shields.io/packagist/vpre/socialiteproviders/scistarter.svg?style=flat-square)](https://packagist.org/packages/socialiteproviders/scistarter)
+[![License](https://img.shields.io/packagist/l/socialiteproviders/scistarter.svg?style=flat-square)](https://packagist.org/packages/socialiteproviders/scistarter)
+
+## Documentation
+
+Full documentation for using this provider can be found at [SciStarter Documentation](http://socialiteproviders.github.io/providers/scistarter/)
---
+++ src/Provider.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace SocialiteProviders\SciStarter;
+
+use Laravel\Socialite\Two\ProviderInterface;
+use SocialiteProviders\Manager\OAuth2\AbstractProvider;
+use SocialiteProviders\Manager\OAuth2\User;
+
+class Provider extends AbstractProvider implements ProviderInterface
+{
+    /**
+     * Unique Provider Identifier.
+     */
+    const IDENTIFIER = 'SCISTARTER';
+
+    /**
+     * {@inheritdoc}
+     */
+    protected $scopes = ['basic'];
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getAuthUrl($state)
+    {
+        return $this->buildAuthUrlFromBase('https://scistarter.com/authorize', $state);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getTokenUrl()
+    {
+        return 'https://scistarter.com/token';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getUserByToken($token)
+    {
+        $response = $this->getHttpClient()->get('', [
+            'headers' => [
+                'Authorization' => 'Bearer '.$token,
+            ],
+        ]);
+
+        return json_decode($response->getBody(), true);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function mapUserToObject(array $user)
+    {
+        return $user;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getTokenFields($code)
+    {
+        return array_merge(parent::getTokenFields($code), [
+            'grant_type' => 'authorization_code'
+        ]);
+    }
+}
